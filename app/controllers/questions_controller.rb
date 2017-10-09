f41f22bce7ff488abaff126f012c79abee12207f
Armaan Amazan <armaan.amazan@gmail.com>
2017-10-09 15:25:49 +0900

    did a thing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+     @query = params[:query]
+     @shit = ""
+     if @query.include? '?'
+        @shit = "Silly question, get dressed and go to work!"
+      elsif @query.include? 'I am going to work right now!'
+        @shit = ""
+      else
+        @shit = "I don't care, get dressed and go to work!"
+      end
+      return @shit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#ask'
 
-  get 'ask', to: 'questions#ask'
+  # get 'ask', to: 'questions#ask'
 
   get 'answer', to: 'questions#answer'
 
